6c2e207bfa0108331716dc3f924646b6b27cc6dd
afkarahmad <137616979+afkarahmad@users.noreply.github.com>
2023-07-18 07:55:22 +0530

    new update

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+       <title>
+       </title>
+       <meta name="viewport"content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet"href="#">
+        <link rel="icon"href="#">
+
+
+
+        <style>
+
+            /*My CSS texts*/
+
+        </style>
+
+    </head>
+     <body>
+
+
+          <!--My HTML body is here-->
+<h1>hello everyone,it is my PHP</h1>
+<?php 
+   
+//oparetors...
+
+
+   $var1 = 78;
+   
+   $newVar = $var1;
+   
+   $newVar *= 2;
+   
+   echo $newVar;
+   echo"<br><br>";
+   echo var_dump(4 <= 5);
+   echo"<br><br>";
+   echo var_dump(4 >= 5);
+      ?>
+
+
+         <script>
+              //my javascript
+
+
+
+
+         </script>
+
+
+
+
+
+
+
+    </body>
+
+
+</html>
